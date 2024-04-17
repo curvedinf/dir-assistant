93947b42c2a869de17d4608712239f60c06ff0fe
Chase Adams <chase.adams@gmail.com>
2024-04-17 17:34:38 -0500

    Added --ignore and DIR_ASSISTANT_GLOBAL_IGNORES option

--- README.md
+++ README.md
@@ -12,6 +12,16 @@ Then run:
 ./setup.sh
 ```
 
+Note: llama-cpp-python's wheel compile was quite buggy for me. You may need to manually change the 
+llama-cpp-python wheel compile options to make it build successfully. To do this, activate the virtualenv 
+(`pyenv activate dir-assistant`) and then run `pip install llama-cpp-python` with the options.
+
+Here is my working formula for a 7900XT on ROCM 6.0.2:
+```
+pyenv activate dir-assistant
+CC=/opt/rocm/llvm/bin/clang CXX=/opt/rocm/llvm/bin/clang++ CMAKE_ARGS="-DLLAMA_HIPBLAS=on -DCMAKE_BUILD_TYPE=Release -DLLAMA_HIPBLAS=ON -DLLAMA_CUDA_DMMV_X=64 -DLLAMA_CUDA_MMV_Y=4 -DCMAKE_PREFIX_PATH=/opt/rocm -DAMDGPU_TARGETS=gfx1100" pip install --no-cache-dir llama-cpp-python
+```
+
 ## Model Download
 
 Download your favorite LLM gguf and place it in the models directory.
--- config.py
+++ config.py
@@ -3,13 +3,11 @@ import os
 
 # Get user inputs
 dir_assistant_model = input("Enter the gguf model filepath relative to dir-assistant/models: ")
-dir_assistant_llama_cpp_options = input("""Enter any options for llama.cpp to run with or press enter for a sane default; 
-reference https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama and format as a json object: 
-""")
+dir_assistant_llama_cpp_options = input("Enter any options for llama.cpp to run with or press enter for a sane default: ")
 if dir_assistant_llama_cpp_options == "":
     dir_assistant_llama_cpp_options = {
         "n_ctx": 0,
-
+        "verbose": False,
     }
 else:
     dir_assistant_llama_cpp_options = json.loads(dir_assistant_llama_cpp_options)
@@ -21,7 +19,8 @@ if dir_assistant_llama_cpp_instructions == "":
 config = {
     "DIR_ASSISTANT_MODEL": dir_assistant_model,
     "DIR_ASSISTANT_LLAMA_CPP_OPTIONS": dir_assistant_llama_cpp_options,
-    "DIR_ASSISTANT_LLAMA_CPP_INSTRUCTIONS": dir_assistant_llama_cpp_instructions
+    "DIR_ASSISTANT_LLAMA_CPP_INSTRUCTIONS": dir_assistant_llama_cpp_instructions,
+    "DIR_ASSISTANT_GLOBAL_IGNORES": ['.git/', '.vscode/', 'node_modules/', 'build/', '.idea/']
 }
 
 # Write to config.json file
@@ -30,3 +29,5 @@ with open(os.path.join(dir_assistant_root, "config.json"), "w") as config_file:
     json.dump(config, config_file, indent=2)
 
 print("Configuration saved to config.json.")
+print('To enable gpu support add "n_gpu_layers": -1 to DIR_ASSISTANT_LLAMA_CPP_OPTIONS')
+print("For more options view https://llama-cpp-python.readthedocs.io/en/latest/api-reference/#llama_cpp.Llama")
