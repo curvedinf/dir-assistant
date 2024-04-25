598edb63eeed592fffb1b5f7a47f69f88e5007e4
Chase Adams <chase.adams@gmail.com>
2024-04-25 17:00:55 -0500

    Organized functions, created a class for the local runner

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import argparse
 import os
 import json
-import sys
-import mimetypes
 
-import numpy as np
-from faiss import IndexFlatL2
 from llama_cpp import Llama
 
 from colorama import Fore, Style
+
+from index import get_files_with_contents, create_file_index, search_index
+from model_runners import LlamaCppRunner
 
 
 def display_startup_art():
@@ -29,84 +28,6 @@ def display_startup_art():
                                                                  
 """ + Style.RESET_ALL)
     print(Style.BRIGHT + Fore.BLUE + "Type 'exit' to quit the conversation.\n\n" + Style.RESET_ALL)
-
-
-def is_text_file(filepath):
-    text_chars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7f})
-    return not bool(open(filepath, 'rb').read(1024).translate(None, text_chars))
-
-
-def get_text_files(directory='.', ignore_paths=[]):
-    text_files = []
-    for root, dirs, files in os.walk(directory):
-        dirs[:] = [d for d in dirs if os.path.join(root, d) not in ignore_paths]
-        for i, filename in enumerate(files, start=1):
-            filepath = os.path.join(root, filename)
-            if os.path.isfile(filepath) and not any(
-                    ignore_path in filepath for ignore_path in ignore_paths) and is_text_file(filepath):
-                text_files.append(filepath)
-    return text_files
-
-
-def get_files_with_contents(directory='.', ignore_paths=[]):
-    files = get_text_files(directory, ignore_paths)
-    files_with_contents = []
-    for i, filepath in enumerate(files, start=1):
-        try:
-            with open(filepath, 'r') as file:
-                contents = file.read()
-        except UnicodeDecodeError:
-            print(f"Skipping {filepath} because it is not a text file.")
-        files_with_contents.append({
-            "filepath": os.path.abspath(filepath),
-            "contents": contents
-        })
-    return files_with_contents
-
-
-def count_tokens(embed, text):
-    return len(embed.tokenize(bytes(text, 'utf-8')))
-
-
-def create_file_index(embed, files_with_contents, embed_chunk_size):
-    # Split the files into chunks
-    chunks = []
-    for i, file_info in enumerate(files_with_contents, start=1):
-        print(f'Indexing file {i}/{len(files_with_contents)}: {file_info["filepath"]}')
-        filepath = file_info["filepath"]
-        contents = file_info["contents"]
-        lines = contents.split('\n')
-        current_chunk = ""
-        start_line_number = 1
-        for line_number, line in enumerate(lines, start=start_line_number):
-            chunk_header = f"---------------\n\nUser file '{filepath}' lines {start_line_number}-{line_number}:\n\n"
-            chunk_add_candidate = current_chunk + line + '\n'
-            chunk_tokens = count_tokens(embed, chunk_header + chunk_add_candidate)
-            if chunk_tokens > embed_chunk_size:
-                chunks.append({"tokens": chunk_tokens, "text": chunk_header + current_chunk})
-                current_chunk = ""
-                start_line_number = line_number  # Update start_line_number
-            else:
-                current_chunk = chunk_add_candidate
-        if current_chunk:  # Add the remaining content as the last chunk
-            chunk_header = f"---------------\n\nUser file '{filepath}' lines {start_line_number}-{len(lines)}:\n\n"
-            chunks.append({"tokens": chunk_tokens, "text": chunk_header + current_chunk})
-
-    # Create the embeddings
-    print("File embeddings created. Total chunks:", len(chunks))
-    print("Max size of an embedding chunk:", embed_chunk_size)
-
-    embeddings = np.array([embed.create_embedding(chunk['text'])['data'][0]['embedding'] for chunk in chunks])
-    index = IndexFlatL2(embeddings.shape[1])
-    index.add(embeddings)
-    return index, chunks
-
-
-def search_index(embed, index, query, all_chunks):
-    query_embedding = embed.create_embedding([query])['data'][0]['embedding']
-    distances, indices = index.search(np.array([query_embedding]), 100)
-    relevant_chunks = [all_chunks[i] for i in indices[0] if i != -1]
-    return relevant_chunks
 
 
 if __name__ == '__main__':
@@ -141,17 +62,7 @@ if __name__ == '__main__':
     ignore_paths.extend(config['DIR_ASSISTANT_GLOBAL_IGNORES'])
     files_with_contents = get_files_with_contents('.', ignore_paths)
     if len(files_with_contents) == 0:
-        print("No files found to index. Exiting...")
-        exit()
-
-    # Initialize the LLM model
-    print("Loading LLM model...")
-    llm = Llama(
-        model_path=llm_model_file,
-        **llama_cpp_options
-    )
-    llama_cpp_llm_context_size = llm.context_params.n_ctx
-    print(f"LLM context size: {llama_cpp_llm_context_size}")
+        print("No files found to index. Running anyway...")
 
     # Initialize the embedding model
     print("Loading embedding model...")
@@ -169,15 +80,20 @@ if __name__ == '__main__':
 
     # Set up the system instructions
     system_instructions = (f"{llama_cpp_instructions}\n\nThe user will ask questions relating \
-to files they will provide. Do your best to answer questions related to the these files. When \
-the user refers to files, always assume they want to know about the files they provided.")
-    system_instructions_tokens = count_tokens(embed, system_instructions)
+    to files they will provide. Do your best to answer questions related to the these files. When \
+    the user refers to files, always assume they want to know about the files they provided.")
 
-    chat_history = [{
-        "role": "system",
-        "content": system_instructions,
-        "tokens": system_instructions_tokens
-    }]
+    # Initialize the LLM model
+    print("Loading local LLM model...")
+    llm = LlamaCppRunner(
+        model_path=llm_model_file,
+        llama_cpp_options=llama_cpp_options,
+        system_instructions=system_instructions,
+        embed=embed,
+        index=index,
+        chunks=chunks,
+        context_file_ratio=context_file_ratio
+    )
 
     # Display the startup art
     display_startup_art()
@@ -188,53 +104,4 @@ the user refers to files, always assume they want to know about the files they p
         user_input = input(Style.BRIGHT + Fore.RED + 'You: \n\n' + Style.RESET_ALL)
         if user_input.lower() == 'exit':
             break
-
-        # Get the relevant chunks and concatenate them. Only use up to the context file ratio of the total tokens
-        relevant_chunks = search_index(embed, index, user_input, chunks)
-        relevant_full_text = ""
-        chunk_total_tokens = 0
-        for i, relevant_chunk in enumerate(relevant_chunks, start=1):
-            chunk_total_tokens += relevant_chunk['tokens']
-            if chunk_total_tokens >= llama_cpp_llm_context_size * context_file_ratio:
-                break
-            relevant_full_text += relevant_chunk['text'] + "\n\n"
-
-        # Add the user input to the chat history
-        user_content = relevant_full_text + user_input
-        chat_history.append({"role": "user", "content": user_content, "tokens": count_tokens(embed, user_input)})
-
-        # Remove old message from the chat history if too large for context
-        sum_of_tokens = sum([message["tokens"] for message in chat_history])
-        while sum_of_tokens > llama_cpp_llm_context_size:
-            chat_history = chat_history.pop(1)  # First history after the system instructions
-            sum_of_tokens = sum([message["tokens"] for message in chat_history])
-
-        # Display the assistant thinking message
-        print(Style.BRIGHT + Fore.GREEN + '\nAssistant: \n' + Style.RESET_ALL)
-        sys.stdout.write(Style.BRIGHT + Fore.WHITE + '\r(thinking...)' + Style.RESET_ALL)
-        sys.stdout.flush()
-
-        # Run the completion
-        completion = llm.create_chat_completion(
-            messages=chat_history,
-            stream=True
-        )
-
-        # Remove the files from the last user message
-        chat_history[-1]["content"] = user_input
-
-        # Display chat history
-        output = {"role": "assistant", "content": "", "tokens": 0}
-        sys.stdout.write(Style.BRIGHT + Fore.WHITE + '\r')
-        for chunk in completion:
-            delta = chunk['choices'][0]['delta']
-            if "content" in delta:
-                output["content"] += delta["content"]
-                sys.stdout.write(delta["content"])
-                sys.stdout.flush()
-        sys.stdout.write(Style.RESET_ALL + '\n\n')
-        sys.stdout.flush()
-
-        # Add the completion to the chat history
-        output["tokens"] = count_tokens(embed, output["content"])
-        chat_history.append(output)
+        llm.stream_chat(user_input)
