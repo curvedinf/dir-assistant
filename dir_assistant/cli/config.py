e6935a1967c8b774d4c097504201e2922778b5d3
Chase Adams <chase.adams@gmail.com>
2025-03-04 11:36:36 -0600

    version bump

--- dir_assistant/cli/config.py
+++ dir_assistant/cli/config.py
@@ -5,7 +5,7 @@ from subprocess import run
 import toml
 from dynaconf import Dynaconf
 
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 CONFIG_FILENAME = "config.toml"
 CONFIG_PATH = join(expanduser("~"), ".config", "dir-assistant")
 STORAGE_PATH = join(expanduser("~"), ".local", "share", "dir-assistant")
