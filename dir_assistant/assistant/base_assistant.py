249bb2994dd219d6d60154695da2cfb8a1359a62
Chase Adams <chase.adams@gmail.com>
2025-06-20 15:38:27 -0500

    merge conflicts

--- dir_assistant/assistant/base_assistant.py
+++ dir_assistant/assistant/base_assistant.py
@@ -88,11 +88,6 @@ class BaseAssistant:
     def call_completion(self, chat_history, is_cgrag_call=False):
         raise NotImplementedError
 
-    def run_completion_generator(
-        self, completion_output, output_message, write_to_stdout
-    ):
-        raise NotImplementedError
-
     def count_tokens(self, text, role="user"):
         raise NotImplementedError
 
@@ -200,9 +195,6 @@ class BaseAssistant:
         tokens = self.count_tokens(response_text, role="assistant")
         return {"role": "assistant", "content": response_text, "tokens": tokens}
 
-    def create_empty_history(self):
-        return {"role": "assistant", "content": "", "tokens": 0}
-
     def cull_history_list(self, history_list):
         total_tokens = sum(h["tokens"] for h in history_list)
         while total_tokens > self.context_size:
@@ -281,3 +273,98 @@ class BaseAssistant:
         if not hasattr(self, "chat_history") or not self.chat_history:
             self.initialize_history()
         self.run_stream_processes(user_input)
+
+    def run_completion_generator(
+        self, completion_output, output_message, write_to_stdout
+    ):
+        thinking_context = self.create_thinking_context(write_to_stdout)
+        for chunk in completion_output:
+            delta = chunk["choices"][0]["delta"]
+            if "content" in delta and delta["content"] != None:
+                output_message["content"] += delta["content"]
+                if (
+                    self.is_done_thinking(thinking_context, output_message["content"])
+                    and write_to_stdout
+                ):
+                    if not self.no_color and self.chat_mode:
+                        sys.stdout.write(
+                            self.get_color_prefix(Style.BRIGHT, Fore.WHITE)
+                        )
+                    extra_delta_after_thinking = self.get_extra_delta_after_thinking(
+                        thinking_context, write_to_stdout
+                    )
+                    if extra_delta_after_thinking is not None:
+                        # Remove the thinking message from the output now that it's complete and
+                        # print the delta after the thinking message
+                        sys.stdout.write(f"\r{' ' * 36}\r")
+                        sys.stdout.write(extra_delta_after_thinking)
+                    sys.stdout.write(delta["content"])
+                    if not self.no_color and self.chat_mode:
+                        sys.stdout.write(self.get_color_suffix())
+                    sys.stdout.flush()
+        return output_message
+    def run_one_off_completion(self, prompt):
+        one_off_history = self.create_one_off_prompt_history(prompt)
+        completion_generator = self.call_completion(one_off_history)
+        output = self.run_completion_generator(
+            completion_generator,
+            self.create_empty_history(role="assistant"),
+            False,  # Set role for assistant
+        )["content"]
+        return self.remove_thinking_message(output)
+    def create_empty_history(self, role="user"):
+        return {"role": role, "content": "", "tokens": 0}
+    def create_one_off_prompt_history(self, prompt):
+        return [
+            {
+                "role": "user",
+                "content": prompt,
+                "tokens": self.count_tokens(prompt, role="user"),
+            }
+        ]
+    def create_thinking_context(self, write_to_stdout):
+        # Create a context instead of using member variables in case
+        # multiple completions are running in parallel
+        if write_to_stdout and self.hide_thinking and self.chat_mode:
+            if not self.no_color:
+                sys.stdout.write(self.get_color_prefix(Style.BRIGHT, Fore.WHITE))
+            sys.stdout.write("(thinking...)")
+            if not self.no_color:
+                sys.stdout.write(self.get_color_suffix())
+            sys.stdout.flush()
+        return {
+            "thinking_start_finished": not self.hide_thinking,
+            "thinking_end_finished": not self.hide_thinking,
+            "delta_after_thinking_finished": None,
+        }
+    def is_done_thinking(self, context, content):
+        if not context["thinking_start_finished"]:
+            # Before the thinking start pattern is found, do not print output
+            if len(content) > len(self.thinking_start_pattern) + 20:
+                context["thinking_start_finished"] = True
+                if self.thinking_start_pattern not in content:
+                    # If the start pattern is not in the output, it's not thinking
+                    context["thinking_end_finished"] = True
+                    context["delta_after_thinking_finished"] = content
+            return False
+        elif not context["thinking_end_finished"]:
+            # While thinking, do not print output
+            if self.thinking_end_pattern in content:
+                context["thinking_end_finished"] = True
+                delta_after_thinking_finished_parts = content.split(
+                    self.thinking_end_pattern
+                )
+                # Get the last part of the string
+                context["delta_after_thinking_finished"] = (
+                    delta_after_thinking_finished_parts[-1]
+                )
+            return False
+        return True
+    def get_extra_delta_after_thinking(self, context, write_to_stdout):
+        # If the thinking is complete, there may be some extra text after the thinking end pattern
+        # This function returns that extra text if it exists.
+        if context["delta_after_thinking_finished"] is not None:
+            output = context["delta_after_thinking_finished"]
+            context["delta_after_thinking_finished"] = None
+            return output
+        return None
